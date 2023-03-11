0db203910a8c1cc9a5747643f158b2cf8c7c049e
vimalds15 <antovimalands@gmail.com>
2023-03-11 10:24:07 +0530

    implemented Product Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>kajfajsdf</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
