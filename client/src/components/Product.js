1b02750e00efa3156350860da54676fa0dedef89
vimalds15 <antovimalands@gmail.com>
2023-03-11 10:38:37 +0530

    implemented product and rating component

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className="my-3 p-3 rounded">
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
@@ -15,15 +16,14 @@ const Product = ({product}) => {
           </Card.Title>
         </a>
 
-        <Card.Text as="div"><div className='my-3'>{product.rating} from {product.numReview} reviews</div></Card.Text>
-
-        <Card.Text as="h3">
-          ${product.price}
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
 
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
