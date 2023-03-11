37fcb40e46dccd714fef115fa4694d832a4de364
vimalds15 <antovimalands@gmail.com>
2023-03-11 12:21:59 +0530

    implemented models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const products = require("./data/products")
+import express from "express"
+import dotenv from "dotenv"
+import colors from "colors"
+import {products} from "./data/products.js"
+import { connectDB } from "./config/db.js"
+
 
 dotenv.config()
-
+connectDB()
 const app = express()
 
 app.get('/',(req,res)=>{
@@ -21,4 +24,4 @@ app.get('/api/products/:id',(req,res)=> {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on PORT ${process.env.PORT}`))
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on PORT ${process.env.PORT}`.yellow.bold))
