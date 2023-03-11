c837ae91bf1e8346a7511e6d579258bade81cad4
vimalds15 <antovimalands@gmail.com>
2023-03-11 12:22:10 +0530

    added data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
-const products = [
+export const products = [
     {
-        _id:"1",
         name:"Airpods Wireless Bluetooth Headphones",
         image:"/images/airpods.jpg",
         description:"Bluetooth technology lets you connect it with compatible devices wirelessly High quality AAC audio offers immersive listening experience Built-in mic allows you to take calls while working",
@@ -12,7 +11,6 @@ const products = [
         numReviews:4,
     },
     {
-        _id:"2",
         name:"iPhone 11 Pro 256GB Memory",
         image:"/images/phone.jpg",
         description:"Introducing the iPhone 11 Pro. A transformative triple-camera system that adds ton of capability without complexity. An unprecedented leap in battery life",
@@ -24,7 +22,6 @@ const products = [
         numReviews:4,
     },
     {
-        _id:"3",
         name:"Canon EOS 80D DSLR Camera",
         image:"/images/camera.jpg",
         description:
@@ -38,7 +35,6 @@ const products = [
         numReviews:3,
     },
     {
-        _id:"4",
         name:"Sony Playstation 4 Pro White Version",
         image:"/images/playstation.jpg",
         description:"The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
@@ -50,7 +46,6 @@ const products = [
         numReviews:3,
     },
     {
-        _id:"5",
         name:"Logitech G-Series Gaming Mouse",
         image:"/images/mouse.jpg",
         description:"Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
@@ -62,7 +57,6 @@ const products = [
         numReviews:2,
     },
     {
-        _id:"6",
         name:"Amazon Echo Dot 3rd Generation",
         image:"/images/alexa.jpg",
         description:"Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
@@ -75,4 +69,3 @@ const products = [
     }
 ]
 
-module.exports=products
