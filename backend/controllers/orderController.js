fe2c4fde0ce9f2d3280a469a45f7fcc289975bab
vimalds15 <antovimalands@gmail.com>
2023-03-13 20:45:50 +0530

    implemented updateOrderToPaid controller and route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,7 @@ import asyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
 
 // @desc Create new order
-// @route POST /apji/orders
+// @route POST /api/orders
 // @access Private
 
 export const addOrderItems = asyncHandler(async (req, res) => {
@@ -37,10 +37,39 @@ export const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Get order details
+// @route GET /api/orders/:id
+// @access Private
+
 export const getOrderById = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id).populate("user", 'name email')
   if(order){
     res.json(order)
+  }else{
+    res.status(404)
+    throw new Error("Order Not Found!!")
+  }
+});
+
+// @desc Update order to paid
+// @route GET /api/orders/:id/pay
+// @access Private
+
+
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  if(order){
+    order.isPaid=true
+    order.paidAt=Date.now()
+    order.paymentResult={
+      id:req.body.id,
+      status:req.body.status,
+      update_time:req.body.update_time,
+      email_address:req.body.payer.email_address
+    }
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
   }else{
     res.status(404)
     throw new Error("Order Not Found!!")
