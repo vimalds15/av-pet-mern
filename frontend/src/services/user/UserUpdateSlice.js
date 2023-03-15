59d57a6dc648db61cd13c3e18203fe8cebc0012f
vimalds15 <antovimalands@gmail.com>
2023-03-15 20:49:44 +0530

    implemented user update slice

--- frontend/src/services/user/UserUpdateSlice.js
+++ frontend/src/services/user/UserUpdateSlice.js
@@ -1,26 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { setUserInfo } from "./UserProfileSlice";
 
 const initialState = {
- success:null,
- loading:null,
- error:null
-}
+  success: null,
+  error: null,
+};
 
 const UserUpdateSlice = createSlice({
   name: "userUpdate",
   initialState,
   reducers: {
-  setSuccess:(state,action) => {
-    state.users = action.payload
-},
-setLoading:(state,action)=>{
-    state.loading=action.payload
-},
-setError:(state,action)=>{
-    state.error=action.payload
-},}
+    setSuccess: (state, action) => {
+      state.users = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
 });
 
-export const {setError,setLoading,setSuccess} = UserUpdateSlice.actions
+export const { setError, setSuccess } = UserUpdateSlice.actions;
 
-export default UserUpdateSlice.reducer
+export default UserUpdateSlice.reducer;
+
+export const updateUser = (user) => async (dispatch, getState) => {
+  try {
+    const { token } = getState().userLogin.userInfo;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(`/api/users/${user._id}`, user, config);
+    dispatch(setSuccess(true));
+    dispatch(setUserInfo(data));
+  } catch (error) {
+    const err =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch(setError(err));
+  }
+};
