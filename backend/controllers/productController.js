d967b84cd19f8b8749f384091426d75030d26d96
vimalds15 <antovimalands@gmail.com>
2023-03-18 21:50:51 +0530

    implemented server top products api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -119,11 +119,19 @@ const createProductReview = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({rating:-1}).limit(3)
+  res.json(products)
+});
+
+
 export {
   getProductById,
   getProducts,
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview
+  createProductReview,
+  getTopProducts
 };
