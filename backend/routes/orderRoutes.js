a17f2b3ed6b8ddc05b79226aad66f7905fdf42ca
vimalds15 <antovimalands@gmail.com>
2023-03-18 15:13:04 +0530

    implemented get all orders route and controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
-import {addOrderItems,getOrderById, updateOrderToPaid,getUserOrders} from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import {addOrderItems,getOrderById, updateOrderToPaid,getUserOrders, getOrders} from "../controllers/orderController.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect,addOrderItems)
+router.route("/").post(protect,addOrderItems).get(protect,admin,getOrders)
 router.route("/myorders").get(protect,getUserOrders)
 router.route("/:id").get(protect,getOrderById)
 router.route("/:id/pay").put(protect,updateOrderToPaid)
