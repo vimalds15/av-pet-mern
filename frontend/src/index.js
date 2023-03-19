a2948e1f3270fd51abd3d59673c6838da5ab85e7
vimalds15 <antovimalands@gmail.com>
2023-03-19 18:58:15 +0530

    degraded to eract v17

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider} from 'react-redux'
@@ -8,15 +8,15 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./services/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
